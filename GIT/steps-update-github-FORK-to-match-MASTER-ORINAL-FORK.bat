a74bfe95c890505260954d137b2e1412d2ee8c22
carloPS4 <carlo.daristotile.ps4@gmail.com>
2017-07-09 19:18:53 -0400

    Update steps-update-github-FORK-to-match-MASTER-ORINAL-FORK.bat

--- GIT/steps-update-github-FORK-to-match-MASTER-ORINAL-FORK.bat
+++ GIT/steps-update-github-FORK-to-match-MASTER-ORINAL-FORK.bat
@@ -1,12 +1,7 @@
-git remote add upstream  https://github.com/akhan118/Coding-Crash-Course.git
-
-git fetch upstream
-
-git pull upstream master
-
-git remote add upstream https://github.com/akhan118/Coding-Crash-Course.git
-
 >git remote add upstream https://github.com/akhan118/Coding-Crash-Course.git
 >git fetch upstream
 >git checkout master
+
+//confused as to which here below
+>git pull upstream master
 >git merge upstream/master
