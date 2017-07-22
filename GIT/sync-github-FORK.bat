e10f400f5c68e138daa7ee160bd33961cb5d4bcc
CarloTonyDaristotile <carlo.daristotile@gmail.com>
2017-07-22 19:52:23 -0400

    Update sync-github-FORK.bat

--- GIT/sync-github-FORK.bat
+++ GIT/sync-github-FORK.bat
@@ -10,3 +10,10 @@ git fetch upstream
 git checkout master
 git reset --hard upstream/master  
 git push origin master --force
+
+// (GITBASH ONLY) GO BACK 1 COMMIT ON YOUR FORK
+// 
+git fetch origin
+git log -3
+git reset --hard HEAD^ 
+git push origin master --force
