576ea59a6d543a636d82224c8bb7d4593f615bf7
CarloTonyDaristotile <carlo.daristotile@gmail.com>
2017-07-22 19:57:31 -0400

    Update sync-github-FORK.bat

--- GIT/sync-github-FORK.bat
+++ GIT/sync-github-FORK.bat
@@ -5,15 +5,28 @@ git fetch upstream
 git checkout master
 git merge upstream/master
 
+/////////////////////////////////////////////////////////////////////////////
 // FOR HARD RESET LOCAL REP WITH UPSTREAM
 git fetch upstream
 git checkout master
+// FIXES YOUR LOCAL TO SAME AS UPSTREAM(akhan/online)
 git reset --hard upstream/master  
+// HARD PUSH YOUR LOCAL(master) TO YOUR ONLINE FORK(origin master)
 git push origin master --force
 
-// (GITBASH ONLY) GO BACK 1 COMMIT ON YOUR FORK
-// 
+/////////////////////////////////////////////////////////////////////////////
+// (GITBASH ONLY) GO BACK 1 COMMIT ON YOUR FORK(origin/master)
+// FETCH FORK(ONLINE) TO YOUR LOCAL
+// UNDO 1 COMMIT LOCALLY
+// AND SEND IT BACK TO FORK(origin/master)
 git fetch origin
 git log -3
 git reset --hard HEAD^ 
+git push origin master --force
+
+/////////////////////////////////////////////////////////////////////////////
+// (GITBASH ONLY) GO BACK 1 COMMIT ON YOUR LOCAL(master)
+// AND SEND IT BACK TO FORK(origin/master)
+git log -3
+git reset --hard HEAD^
 git push origin master --force
