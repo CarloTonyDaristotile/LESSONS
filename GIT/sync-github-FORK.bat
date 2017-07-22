358aa1a00c43d413b33563fa265cc96efdac83ab
CarloTonyDaristotile <carlo.daristotile@gmail.com>
2017-07-22 18:51:18 -0400

    Update sync-github-FORK.bat

--- GIT/sync-github-FORK.bat
+++ GIT/sync-github-FORK.bat
@@ -1,12 +1,12 @@
 git remote add upstream  https://github.com/akhan118/Coding-Crash-Course.git
 
+// FOR MERGING LOCAL REP WITH UPSTREAM
 git fetch upstream
+git checkout master
+git merge upstream/master
 
-git pull upstream master
-
-git remote add upstream https://github.com/ak...­
-
->git remote add upstream https://github.com/ak...­
->git fetch upstream
->git checkout master
->git merge upstream/master
+// FOR HARD RESET LOCAL REP WITH UPSTREAM
+git fetch upstream
+git checkout master
+git reset --hard upstream/master  
+git push origin master --force
